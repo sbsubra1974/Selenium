583391c22ddab22d8927d5ce6669c6e8e1656834
subramanian raghavan <sbsubra1974@gmail.com>
2020-05-07 19:53:08 +0530

    included the command webdriver = new RemoteWebDriver(new URL("http://192.168.1.5:4444"), chromeOptions); in test class SeleniumWebDriverUploadFilePageObjectTest

--- maven-demo/src/test/java/com/selenium/testng/SeleniumWebDriverUploadFilePageObjectTest.java
+++ maven-demo/src/test/java/com/selenium/testng/SeleniumWebDriverUploadFilePageObjectTest.java
@@ -5,9 +5,15 @@ import org.testng.annotations.Test;
 import com.selenium.po.SeleniumWebDriverUploadFilePageObject;
 
 import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 
 public class SeleniumWebDriverUploadFilePageObjectTest {
@@ -23,9 +29,11 @@ public class SeleniumWebDriverUploadFilePageObjectTest {
 	  
   }
   @BeforeClass(groups= {"gp1"})
-  public void beforeClass() {
+  public void beforeClass() throws MalformedURLException {
   System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-  webdriver = new ChromeDriver();
+  //webdriver = new ChromeDriver();
+  ChromeOptions chromeOptions = new ChromeOptions();
+  webdriver = new RemoteWebDriver(new URL("http://192.168.1.5:4444"), chromeOptions);
   pgobj= new SeleniumWebDriverUploadFilePageObject();
   actions = new Actions(webdriver);  
   
